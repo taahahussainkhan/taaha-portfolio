7dff5dfe5c82ee1643bace180dfb1ba3a4c11ccc
taahahussainkhan <taaha128@gmail.com>
2024-08-19 11:01:44 +0500

    contact form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,42 @@
-import React from "react"
-const Contact = () => {
+"use client";
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
+import { motion } from "framer-motion";
+
+
+function ContactForm() {
+  const [state, handleSubmit] = useForm("xanwbzdo");
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>;
+  }
   return (
-    <>Contact page</>
-  )
+    <div className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-6 w-[420px] align-center justify-center">
+        {/* <label htmlFor="email" >Email Address</label> */}
+        <input type="text"  placeholder="Name"name="name" className="bg-[#27272c] text-white h-14 text-center"/>
+        <input type="text" name="phone no" placeholder="Phone Number" className="bg-[#27272c] text-white h-14"/>
+        <input id="email" type="email" name="email" className="flex items-center justify-center bg-[#27272c] text-white h-14" placeholder="Email"/>
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
+        <textarea id="message" name="message"className="bg-[#27272c] text-white h-[150px]"
+        placeholder="Message"
+        />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+        <motion.button
+          type="submit"
+          disabled={state.submitting}
+          className="text-white font-bold border border-accent-hover bg-accent-hover h-14"
+          whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+        >
+          Submit
+        </motion.button>
+      </form>
+    </div>
+  );
 }
-export default Contact
+
+export default ContactForm;
